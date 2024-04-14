06382475f033524de73bd7f136e4b9accf5dd68b
kimscastle <uiskim22@pos.idserve.net>
2024-04-14 19:23:13 +0900

    [ADD] 프로젝트생성

---
+++ Genti/Genti/GentiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GentiApp.swift
+//  Genti
+//
+//  Created by uiskim on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GentiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
