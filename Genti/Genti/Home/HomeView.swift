1f6550b524b4c11a27440df55376606ef09dc328
kimscastle <uiskim22@pos.idserve.net>
2024-05-25 11:24:52 +0900

    [FEAT] Feed component 작성

--- Genti/Genti/Home/HomeView.swift
+++ Genti/Genti/Home/HomeView.swift
@@ -2,95 +2,22 @@
 //  HomeView.swift
 //  Genti
 //
-//  Created by uiskim on 4/18/24.
+//  Created by uiskim on 5/25/24.
 //
 
 import SwiftUI
 
 struct HomeView: View {
-    var feeds: [Feed] = Feed.mocks
-    @State private var scrollViewOffset: CGFloat  = 0
     var body: some View {
-        ZStack {
-            VStack {
-                Color.green3
-                Color.backgroundWhite
-            }
-            ScrollView {
-                VStack(spacing: 18) {
-                    headerView()
-                    feedView()
-                } //:VSTACK
-                .padding(.bottom, 120)
-                .background(.backgroundWhite)
-            } //: SCROLLVIEW
-            .scrollIndicators(.hidden)
-            
-            if scrollViewOffset < 20 {
-                uploadButton()
-                    .transition(AnyTransition(.move(edge: .top).combined(with: .opacity)))
-            }
+        ScrollView {
+            VStack(spacing: 0) {
+                FeedView()
+                FeedView(description: Constants.text(length: 100))
+                
+                FeedView(description: Constants.text(length: 100))
+                
+            } //:VSTACK
         }
-        .ignoresSafeArea()
-        .overlay(alignment: .top) {
-            if scrollViewOffset < 20 {
-                Rectangle()
-                    .frame(height: 0)
-                    .background(.backgroundWhite)
-                    .transition(.fade)
-            }
-        }
-        .animation(.snappy, value: scrollViewOffset)
-    }
-    
-    private func headerView() -> some View {
-        ZStack(alignment: .bottom) {
-            Image("Home_navigationBG")
-            HStack {
-                Image("Genti_logo_black")
-                Spacer()
-                Image("Upload")
-                    .background(.black.opacity(0.001))
-                    .onTapGesture {
-                        print("업로드버튼 눌림")
-                    }
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 28)
-            .padding(.bottom, 10)
-            .readingFrame { frame in
-                scrollViewOffset = frame.minY
-            }
-        }
-    }
-    
-    private func feedView() -> some View {
-        LazyVStack(spacing: 18) {
-            ForEach(feeds) {
-                FeedView(
-                    profileImage: $0.profileImage,
-                    userName: $0.userName,
-                    mainImage: $0.mainImage,
-                    description: $0.description,
-                    likeCount: Int.random(in: 1...100)) {
-                        
-                    }
-                    .padding(.horizontal, 16)
-            }
-        }
-    }
-    
-    private func uploadButton() -> some View {
-        Image("Upload_Stroke")
-            .frame(width: 60, height: 60)
-            .background(.ultraThinMaterial)
-            .clipShape(Circle())
-            .onTapGesture {
-                print("업로드버튼 눌림")
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-            .padding(.top, 80)
-            .padding(.trailing, 20)
     }
 }
 
--- Genti/Genti/Home/Model/Feed.swift
+++ Genti/Genti/Home/Model/Feed.swift
@@ -9,19 +9,17 @@ import Foundation
 
 struct Feed: Identifiable {
     let id: String = UUID().uuidString
-    let profileImage: String = Constants.randomImage
-    let userName: String
     let mainImage: String
     let description: String
     
     static let mocks: [Feed] = [
-        .init(userName: "My_Youth4", mainImage: "SampleImage32", description: Constants.text(length: 50)),
-        .init(userName: "My_Youth1", mainImage: "SampleImage23", description: Constants.text(length: 20)),
-        .init(userName: "My_Youth2", mainImage: "SampleImage23", description: Constants.text(length: 80)),
-        .init(userName: "My_Youth3", mainImage: "SampleImage32", description: Constants.text(length: 40)),
-        .init(userName: "My_Youth5", mainImage: "SampleImage23", description: Constants.text(length: 120)),
-        .init(userName: "My_Youth6", mainImage: "SampleImage23", description: Constants.text(length: 14)),
-        .init(userName: "My_Youth7", mainImage: "SampleImage32", description: Constants.text(length: 54)),
-        .init(userName: "My_Youth8", mainImage: "SampleImage23", description: Constants.text(length: 100))
+        .init(mainImage: "SampleImage32", description: Constants.text(length: 50)),
+        .init(mainImage: "SampleImage23", description: Constants.text(length: 20)),
+        .init(mainImage: "SampleImage23", description: Constants.text(length: 80)),
+        .init(mainImage: "SampleImage32", description: Constants.text(length: 40)),
+        .init(mainImage: "SampleImage23", description: Constants.text(length: 120)),
+        .init(mainImage: "SampleImage23", description: Constants.text(length: 14)),
+        .init(mainImage: "SampleImage32", description: Constants.text(length: 54)),
+        .init(mainImage: "SampleImage23", description: Constants.text(length: 200))
     ]
 }
