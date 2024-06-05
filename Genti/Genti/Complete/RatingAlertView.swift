3cb5f692dbe87f52e81875a7e2d626b644b80661
kimscastle <uiskim22@pos.idserve.net>
2024-06-05 17:51:07 +0900

    [FIX] 코드정리(#26)

--- Genti/Genti/Complete/RatingAlertView.swift
+++ Genti/Genti/Complete/RatingAlertView.swift
@@ -16,69 +16,13 @@ struct RatingAlertView: View {
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
-                VStack(spacing: 12) {
-                    Text("사진은 어떠셨나요?")
-                        .pretendard(.headline4)
-                    
-                    if showErrorText {
-                        if rating == 0 {
-                            Text("별점은 1점부터 줄 수 있어요!")
-                                .pretendard(.description)
-                                .foregroundStyle(.red)
-                        }
-                    }
-                    
-                    Text("별점을 남겨주시면 앞으로 더 마음에 드는\n사진을 만들어 드릴 수 있어요!")
-                        .pretendard(.description)
-                        .multilineTextAlignment(.center)
-                    HStack(spacing: 0) {
-                        ForEach(1..<6) { index in
-                            Image(index > rating ? "Star_empty" : "Star_fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                                .onTapGesture {
-                                    rating = index
-                                }
-                        }
-                    }
-                } //:VSTACK
-                .padding(.top, 19)
-                .padding(.bottom, 15)
+                ratingContentView()
+
                 Rectangle()
                     .frame(height: 1)
                 
-                Text("별점 남기기")
-                    .pretendard(.headline4)
-                    .padding(.top, 7)
-                    .padding(.bottom, 13)
-                    .frame(maxWidth: .infinity)
-                    .background(.black.opacity(0.001))
-                    .onTapGesture {
-                        
-                        if rating == 0 {
-                            self.showErrorText = true
-                        } else {
-                            Task {
-                                self.showErrorText = false
-                                self.isLoading = true
-                                try await Task.sleep(nanoseconds: 2000000000)
-                                self.isLoading = false
-                                print("별점 : \(self.rating)점")
-                                self.dismiss()
-                            }
-                        }
-                    }
-                    
-                Text("건너뛰기")
-                    .pretendard(.description)
-                    .underline()
-                    .padding(.bottom, 13)
-                    .frame(maxWidth: .infinity)
-                    .background(.black.opacity(0.001))
-                    .onTapGesture {
-                        self.dismiss()
-                    }
+                acceptButton()
+                cancelButton()
             }
             .background(.gray4)
             .clipShape(.rect(cornerRadius: 14))
@@ -88,11 +32,88 @@ struct RatingAlertView: View {
                 LoadingView()
             }
         }
-
         .presentationBackground {
             Color.black.opacity(0.4)
                 .ignoresSafeArea()
         }
+    }
+    
+    private func ratingContentView() -> some View {
+        VStack(spacing: 12) {
+            titleView()
+            ratingStarView()
+        } //:VSTACK
+        .padding(.top, 19)
+        .padding(.bottom, 15)
+    }
+    private func cancelButton() -> some View {
+        Text("건너뛰기")
+            .pretendard(.description)
+            .foregroundStyle(.black)
+            .underline()
+            .padding(.bottom, 13)
+            .frame(maxWidth: .infinity)
+            .background(.black.opacity(0.001))
+            .onTapGesture {
+                self.dismiss()
+            }
+    }
+    private func acceptButton() -> some View {
+        Text("별점 남기기")
+            .pretendard(.headline4)
+            .foregroundStyle(.black)
+            .padding(.top, 7)
+            .padding(.bottom, 13)
+            .frame(maxWidth: .infinity)
+            .background(.black.opacity(0.001))
+            .onTapGesture {
+                
+                if rating == 0 {
+                    self.showErrorText = true
+                } else {
+                    Task {
+                        self.showErrorText = false
+                        self.isLoading = true
+                        try await Task.sleep(nanoseconds: 2000000000)
+                        self.isLoading = false
+                        print("별점 : \(self.rating)점")
+                        self.dismiss()
+                    }
+                }
+            }
+    }
+    private func ratingStarView() -> some View {
+        HStack(spacing: 0) {
+            ForEach(1..<6) { index in
+                Image(index > rating ? "Star_empty" : "Star_fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30)
+                    .onTapGesture {
+                        rating = index
+                    }
+            }
+        }
+    }
+    private func titleView() -> some View {
+        VStack(spacing: 12) {
+            Text("사진은 어떠셨나요?")
+                .pretendard(.headline4)
+                .foregroundStyle(.black)
+            
+            if showErrorText {
+                if rating == 0 {
+                    Text("별점은 1점부터 줄 수 있어요!")
+                        .pretendard(.description)
+                        .foregroundStyle(.red)
+                }
+            }
+            
+            Text("별점을 남겨주시면 앞으로 더 마음에 드는\n사진을 만들어 드릴 수 있어요!")
+                .pretendard(.description)
+                .foregroundStyle(.black)
+                .multilineTextAlignment(.center)
+        } //:VSTACK
     }
 }
 
