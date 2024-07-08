24989a1c4745928bef271e4070e87d3e58efecbc
kimscastle <uiskim22@pos.idserve.net>
2024-07-08 17:16:54 +0900

    [FIX] zstack depth줄일수있는부분 줄이기(#41)

--- Genti_iOS/Genti_iOS/Complete/Component/HorizontalImageContentView.swift
+++ Genti_iOS/Genti_iOS/Complete/Component/HorizontalImageContentView.swift
@@ -12,7 +12,60 @@ struct HorizontalImageContentView: View {
     @Bindable var viewModel: PhotoCompleteViewViewModel
     
     var body: some View {
-        ZStack(alignment: .top) {
+        VStack(spacing: 0) {
+            Image(.gentiLOGO)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 44)
+                .padding(.top, 80)
+            
+            Image(.charactor)
+                .resizable( )
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 162)
+                .padding(.top, 12)
+            
+            VStack {
+                HStack(spacing: 0) {
+                    Text("하나뿐인 나만의 사진")
+                        .pretendard(.headline1)
+                        .foregroundStyle(.green1)
+                    
+                    Text("이")
+                        .pretendard(.headline3)
+                }
+                Text("완성되었어요!")
+                    .pretendard(.headline3)
+            }
+            .frame(height: 57)
+            .foregroundStyle(.black)
+            
+            Spacer()
+                .frame(height: 16)
+            
+            
+            viewModel.getImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 212)
+                .addDownloadButton { self.viewModel.sendAction(.downloadButtonTap) }
+                .cornerRadiusWithGradient()
+                .onTapGesture {
+                    self.viewModel.sendAction(.imageTap)
+                }
+                .onAppear {
+                    self.viewModel.sendAction(.viewWillAppear)
+                }
+            
+            Spacer().frame(height: 18)
+            
+            Text("사진이 마이페이지에 저장되었어요!")
+                .pretendard(.small)
+                .foregroundStyle(.gray3)
+        }
+        .frame(height: 640)
+        .frame(maxWidth: .infinity)
+        .background(alignment: .top) {
             Rectangle()
                 .fill(.backgroundWhite)
                 .clipShape(.rect(cornerRadius: 30))
@@ -21,71 +74,6 @@ struct HorizontalImageContentView: View {
                     LinearGradient(colors: [.gradientPurple2, .gentiPurple.opacity(0)], startPoint: .top, endPoint: .bottom)
                         .frame(height: 316)
                 }
-            // Content
-            VStack(spacing: 0) {
-                    Image("Genti_LOGO")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 44)
-                        .padding(.top, 80)
-                
-                    Image("Charactor")
-                        .resizable( )
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 162)
-                        .padding(.top, 12)
-                    
-                    VStack {
-                        HStack(spacing: 0) {
-                            Text("하나뿐인 나만의 사진")
-                                .pretendard(.headline1)
-                                .foregroundStyle(.green1)
-                            Text("이")
-                                .pretendard(.headline3)
-                        }
-                        Text("완성되었어요!")
-                            .pretendard(.headline3)
-                    }
-                    .frame(height: 57)
-                    .foregroundStyle(.black)
-                    
-                    Spacer()
-                        .frame(height: 16)
-                    
-                viewModel.getImage
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 212)
-                    .overlay(alignment: .bottomTrailing) {
-                        Image("Download")
-                            .resizable()
-                            .frame(width: 44, height: 44)
-                            .padding(.bottom, 6)
-                            .padding(.trailing, 18)
-                            .onTapGesture {
-                                print(#fileID, #function, #line, "- downloadtap")
-                            }
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .strokeBorder(LinearGradient(colors: [.gentiGreen, .gentiGreen.opacity(0)], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2)
-                    )
-                    .onTapGesture {
-                        print(#fileID, #function, #line, "- imagetap")
-                        self.viewModel.sendAction(.imageTap)
-                    }
-                    
-                    Spacer()
-                        .frame(height: 18)
-                    Text("사진이 마이페이지에 저장되었어요!")
-                        .pretendard(.small)
-                        .foregroundStyle(.gray3)
-                }
-        } //:ZSTACK
-        .frame(height: 640)
-        .onAppear {
-            self.viewModel.sendAction(.viewWillAppear)
         }
     }
 }
@@ -93,3 +81,9 @@ struct HorizontalImageContentView: View {
 //#Preview {
 //    HorizontalImageContentView(viewModel: PhotoCompleteViewViewModel(router: .init()))
 //}
+
+
+
+
+
+
--- Genti_iOS/Genti_iOS/Complete/Component/VerticalImageContentView.swift
+++ Genti_iOS/Genti_iOS/Complete/Component/VerticalImageContentView.swift
@@ -11,7 +11,57 @@ struct VerticalImageContentView: View {
     @Bindable var viewModel: PhotoCompleteViewViewModel
     
     var body: some View {
-        ZStack(alignment: .top) {
+        VStack(spacing: 0) {
+            Image("Genti_LOGO")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 44)
+                .padding(.top, 80)
+            
+            
+            VStack {
+                HStack(spacing: 0) {
+                    Text("하나뿐인 나만의 사진")
+                        .pretendard(.headline1)
+                        .foregroundStyle(.green1)
+                    Text("이")
+                        .pretendard(.headline3)
+                }
+                Text("완성되었어요!")
+                    .pretendard(.headline3)
+            }
+            .frame(height: 57)
+            .foregroundStyle(.black)
+            .padding(.top, 29)
+            
+            Spacer()
+                .frame(height: 16)
+            
+            viewModel.getImage
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 360)
+                .addDownloadButton { self.viewModel.sendAction(.downloadButtonTap) }
+                .cornerRadiusWithGradient()
+                .onTapGesture {
+                    print(#fileID, #function, #line, "- imagetap")
+                    self.viewModel.sendAction(.imageTap)
+                }
+                .onAppear {
+                    self.viewModel.sendAction(.viewWillAppear)
+                }
+            
+            
+            Spacer()
+                .frame(height: 18)
+            Text("사진이 마이페이지에 저장되었어요!")
+                .pretendard(.small)
+                .foregroundStyle(.gray3)
+        }
+        .frame(height: 640)
+        .frame(maxWidth: .infinity)
+        
+        .background(alignment: .top) {
             Rectangle()
                 .fill(.backgroundWhite)
                 .clipShape(.rect(cornerRadius: 30))
@@ -20,67 +70,6 @@ struct VerticalImageContentView: View {
                     LinearGradient(colors: [.gradientPurple2, .gentiPurple.opacity(0)], startPoint: .top, endPoint: .bottom)
                         .frame(height: 171)
                 }
-            // Content
-            VStack(spacing: 0) {
-                    Image("Genti_LOGO")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 44)
-                        .padding(.top, 80)
-                
-                    
-                    VStack {
-                        HStack(spacing: 0) {
-                            Text("하나뿐인 나만의 사진")
-                                .pretendard(.headline1)
-                                .foregroundStyle(.green1)
-                            Text("이")
-                                .pretendard(.headline3)
-                        }
-                        Text("완성되었어요!")
-                            .pretendard(.headline3)
-                    }
-                    .frame(height: 57)
-                    .foregroundStyle(.black)
-                    .padding(.top, 29)
-                    
-                    Spacer()
-                        .frame(height: 16)
-                    
-                viewModel.getImage
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 360)
-                    .overlay(alignment: .bottomTrailing) {
-                        Image("Download")
-                            .resizable()
-                            .frame(width: 44, height: 44)
-                            .padding(.bottom, 12)
-                            .padding(.trailing, 22)
-                            .onTapGesture {
-                                print(#fileID, #function, #line, "- downloadtap")
-                            }
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .strokeBorder(LinearGradient(colors: [.gentiGreen, .gentiGreen.opacity(0)], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2)
-                    )
-                    .onTapGesture {
-                        print(#fileID, #function, #line, "- imagetap")
-                        self.viewModel.sendAction(.imageTap)
-                    }
-                    
-                    Spacer()
-                        .frame(height: 18)
-                    Text("사진이 마이페이지에 저장되었어요!")
-                        .pretendard(.small)
-                        .foregroundStyle(.gray3)
-                }
-        } //:ZSTACK
-        .frame(height: 640)
-        .onAppear {
-            self.viewModel.sendAction(.viewWillAppear)
         }
     }
 }
