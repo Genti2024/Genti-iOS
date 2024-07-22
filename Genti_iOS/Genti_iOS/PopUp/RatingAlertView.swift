40b02a41a7fb7740b69118c52c0be77220aa16e9
kimscastle <uiskim22@pos.idserve.net>
2024-07-22 13:38:25 +0900

    [FIX] 폴더정리(#main)

--- Genti_iOS/Genti_iOS/PopUp/RatingAlertView.swift
+++ Genti_iOS/Genti_iOS/PopUp/RatingAlertView.swift
@@ -7,54 +7,6 @@
 
 import SwiftUI
 
-@Observable
-final class RatingAlertViewModel: ViewModel {
-    
-    var userRepository: UserRepository
-    
-    struct State {
-        var rating: Int = 0
-        var isLoading: Bool = false
-    }
-    
-    enum Input {
-        case ratingViewSkipButtonTap
-        case ratingViewSubmitButtonTap
-        case ratingViewStarTap(rating: Int)
-    }
-    
-    var state: State
-    
-    init(userRepository: UserRepository) {
-        self.userRepository = userRepository
-        self.state = .init()
-    }
-    
-    func sendAction(_ input: Input) {
-        switch input {
-        case .ratingViewSkipButtonTap:
-            NotificationCenter.default.post(name: Notification.Name(rawValue: "ratingCompleted"), object: nil)
-        case .ratingViewSubmitButtonTap:
-            Task {
-                do {
-                    await MainActor.run {
-                        self.state.isLoading = true
-                    }
-                    try await Task.sleep(nanoseconds: 1000000000)
-                    try await userRepository.ratePhoto(rate: state.rating)
-                    
-                    await MainActor.run {
-                        self.state.isLoading = false
-                        NotificationCenter.default.post(name: Notification.Name(rawValue: "ratingCompleted"), object: nil)
-                    }
-                }
-            }
-        case .ratingViewStarTap(let rating):
-            self.state.rating = rating
-        }
-    }
-}
-
 struct RatingAlertView: View {
     @State var viewModel: RatingAlertViewModel
     
