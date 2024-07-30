60ee8d8469afad312a5c7e34c1881bb2088d1957
kimscastle <uiskim22@pos.idserve.net>
2024-07-30 12:33:43 +0900

    [FIX] 스플래시임시구현(용량너무큼)(#53)

--- Genti_iOS/Genti_iOS/SplashView/SplashView.swift
+++ Genti_iOS/Genti_iOS/SplashView/SplashView.swift
@@ -12,64 +12,27 @@ import Lottie
 
 struct SplashView: View {
     @Bindable var router: Router<MainRoute>
-    @State private var play: Bool = false
     var body: some View {
-        ALottieView(animationName: "SplashLottie", play: $play) {
-            router.routeTo(.login)
-        }
-            .ignoresSafeArea()
-            .onAppear {
-                self.play = true
-            }
-    }
-}
+        
+        ZStack {
+            // Background Color
+            Color.backgroundWhite
+                .ignoresSafeArea()
+            // Content
+            Text("스플래시뷰입니다")
+                .foregroundStyle(.black)
+                .pretendard(.headline1)
+                .onAppear {
+                    Task {
+                        do {
+                            try await Task.sleep(nanoseconds: 1000000000)
+                            await MainActor.run {
+                                router.routeTo(.login)
+                            }
+                        }
 
-
-struct ALottieView: UIViewRepresentable {
-    let animationName: String
-    @Binding var play: Bool
-    var animationView: LottieAnimationView
-    var onComplete: (() -> Void)?
-
-    init(animationName: String,
-         play: Binding<Bool> = .constant(true),
-         onComplete: (() -> Void)? = nil
-    ) {
-        self.animationName = animationName
-        self.animationView = LottieAnimationView(name: animationName)
-        self._play = play
-        self.onComplete = onComplete
-    }
-
-    class Coordinator: NSObject {
-        @Binding var play: Bool
-        let parent: ALottieView
-
-        init(parent: ALottieView, play: Binding<Bool>) {
-            self.parent = parent
-            _play = play
-        }
-    }
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(parent: self, play: $play)
-    }
-
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: .zero)
-        view.addSubview(animationView)
-        animationView.translatesAutoresizingMaskIntoConstraints = false
-        animationView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        animationView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        return view
-    }
-
-    func updateUIView(_ uiView: UIView, context: Context) {
-        if play {
-            animationView.play { [self] _ in
-                play.toggle()
-                onComplete?()
-            }
-        }
+                    }
+                }
+        } //:ZSTACK
     }
 }
