564c36554544a9f4fb5acc7ecb72454f6582045a
kimscastle <uiskim22@pos.idserve.net>
2024-08-08 14:28:13 +0900

    [FIX] 이미지생성요청뷰 usecase적용(#69)

--- Genti_iOS/Genti_iOS/Generator/ViewModels/ThirdGeneratorViewModel.swift
+++ Genti_iOS/Genti_iOS/Generator/ViewModels/ThirdGeneratorViewModel.swift
@@ -11,6 +11,19 @@ import Photos
 @Observable 
 final class ThirdGeneratorViewModel: ViewModel, GetImageFromImagePicker {
 
+    let imageGenerateUseCase: ImageGenerateUseCase
+    var state: ThirdGeneratorViewModel.State
+    var requestImageData: RequestImageData
+    var router: Router<MainRoute>
+    
+    init(imageGenerateUseCase: ImageGenerateUseCase, requestImageData: RequestImageData, router: Router<MainRoute>) {
+        self.imageGenerateUseCase = imageGenerateUseCase
+        self.requestImageData = requestImageData
+        self.router = router
+        self.state = .init()
+    }
+    
+    
     struct State {
         var referenceImages: [ImageAsset] = []
         var isLoading: Bool = false
@@ -43,7 +56,6 @@ final class ThirdGeneratorViewModel: ViewModel, GetImageFromImagePicker {
         do {
             state.isLoading = true
             try await imageGenerateUseCase.requestImage(from: self.requestData())
-            NotificationCenter.default.post(name: Notification.Name(rawValue: "profileReload"), object: nil)
             state.isLoading = false
             router.routeTo(.requestCompleted)
         } catch(let error) {
@@ -54,17 +66,6 @@ final class ThirdGeneratorViewModel: ViewModel, GetImageFromImagePicker {
             }
             state.showAlert = .reportGentiError(error: error, action: nil)
         }
-    }
-    
-    let imageGenerateUseCase: ImageGenerateUseCase
-    var state: ThirdGeneratorViewModel.State
-    var requestImageData: RequestImageData
-    var router: Router<MainRoute>
-    init(imageGenerateUseCase: ImageGenerateUseCase, requestImageData: RequestImageData, router: Router<MainRoute>) {
-        self.imageGenerateUseCase = imageGenerateUseCase
-        self.requestImageData = requestImageData
-        self.router = router
-        self.state = .init()
     }
     
     func setReferenceImageAssets(assets: [ImageAsset]) {
