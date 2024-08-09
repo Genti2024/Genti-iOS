cc901e30093b316f426981802ca5ebabf92aaf22
kimscastle <uiskim22@pos.idserve.net>
2024-08-09 12:45:43 +0900

    [HOTFIX] 프로필업데이트시점변경(#main)

--- Genti_iOS/Genti_iOS/Domain/UseCases/ImageGenerateUseCase.swift
+++ Genti_iOS/Genti_iOS/Domain/UseCases/ImageGenerateUseCase.swift
@@ -26,7 +26,8 @@ final class ImageGenerateUseCaseImpl: ImageGenerateUseCase {
         guard let selectedAngle = userData.selectedAngle, let selectedFrame = userData.selectedFrame, let selectedRatio = userData.selectedRatio else {
             throw GentiError.clientError(code: "Unwrapping", message: "각도,프레임,비율의 값이 비어있습니다")
         }
-        NotificationCenter.default.post(name: Notification.Name(rawValue: "profileReload"), object: nil)
         try await generateRepository.requestGenerateImage(prompt: userData.description, poseURL: referenceS3Key, faceURLs: facesS3Keys, angle: selectedAngle, coverage: selectedFrame, ratio: selectedRatio)
+        
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "profileReload"), object: nil)
     }
 }
