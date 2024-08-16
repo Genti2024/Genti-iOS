560a457ae8995675599f7f4d3413add9e6fcd8dc
kimscastle <uiskim22@pos.idserve.net>
2024-08-16 09:40:46 +0900

    [FEAT] 추가적인 유저정보 amplitude에 저장(#77)

--- Genti_iOS/Genti_iOS/Instructure/EventLog/Model/LogUserPropertyType.swift
+++ Genti_iOS/Genti_iOS/Instructure/EventLog/Model/LogUserPropertyType.swift
@@ -9,6 +9,8 @@ import Foundation
 
 enum LogUserPropertyType {
     case userEmail(email: String)
+    case userGender(gender: Gender)
+    case userBirthYear(birthYear: Int)
     case shareButtonTap
     case downloadPhoto
     case scrollMainView
@@ -26,6 +28,10 @@ enum LogUserPropertyType {
             return "user_main_scroll"
         case .refreshMainView:
             return "user_promptsuggest_refresh"
+        case .userGender:
+            return "user_sex"
+        case .userBirthYear(birthYear: let birthYear):
+            return "user_birthYear"
         }
     }
     
@@ -33,6 +39,10 @@ enum LogUserPropertyType {
         switch self {
         case .userEmail(let email):
             return ["email": email]
+        case .userGender(let gender):
+            return ["sex": gender.rawValue]
+        case .userBirthYear(let birthYear):
+            return ["birthYear": birthYear]
         default:
             return nil
         }
