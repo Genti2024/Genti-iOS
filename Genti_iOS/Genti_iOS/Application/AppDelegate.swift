286981a6ebd5e9ad892fea69d8f0a7cba94d32b3
kimscastle <uiskim22@pos.idserve.net>
2024-08-24 22:21:48 +0900

    [FEAT] 푸시알림설정

--- Genti_iOS/Genti_iOS/Application/AppDelegate.swift
+++ Genti_iOS/Genti_iOS/Application/AppDelegate.swift
@@ -17,6 +17,8 @@ import KakaoSDKAuth
 @main
 class AppDelegate: NSObject, UIApplicationDelegate {
     
+    let userdefaultsRepository = UserDefaultsRepositoryImpl()
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         if (AuthApi.isKakaoTalkLoginUrl(url)) {
             return AuthController.handleOpenUrl(url: url)
@@ -43,22 +45,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         guard let identify = identify else { return true }
         Amplitude.instance().identify(identify)
         
-        
-        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
         UNUserNotificationCenter.current().delegate = self
-        
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
-        UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-        )
-        
-        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
         application.registerForRemoteNotifications()
-        
-        // 파이어베이스 Meesaging 설정
         Messaging.messaging().delegate = self
-        
         return true
     }
 }
@@ -89,8 +78,10 @@ extension AppDelegate: MessagingDelegate {
     
     // 파이어베이스 MessagingDelegate 설정
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        guard let fcmToken = fcmToken else { return }
+        userdefaultsRepository.setFcmToken(token: fcmToken)
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
 }
