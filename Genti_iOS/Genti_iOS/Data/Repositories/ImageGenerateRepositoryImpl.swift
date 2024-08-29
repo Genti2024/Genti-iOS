f189c5533485ed33efda9eff2a78a7984e2f464b
kimscastle <uiskim22@pos.idserve.net>
2024-08-29 18:17:15 +0900

    [FIX] 사진업로드 async let으로 변경

--- Genti_iOS/Genti_iOS/Data/Repositories/ImageGenerateRepositoryImpl.swift
+++ Genti_iOS/Genti_iOS/Data/Repositories/ImageGenerateRepositoryImpl.swift
@@ -22,36 +22,25 @@ final class ImageGenerateRepositoryImpl: ImageGenerateRepository {
     
     func getS3Key(from phAsset: PHAsset?) async throws -> String? {
         guard let phAsset = phAsset else { return nil }
-        do {
-            let fileName = phAsset.value(forKey: "filename") as! String
-            let response: GetUploadImageUrlDTO = try await self.requsetService.fetchResponse(for: GeneratorRouter.getPresignedUrl(fileName: fileName))
-            let imageData = try await imageDataTransferService.requestImageData(for: phAsset)
-            return try await uploadService.upload(s3Key: response.s3Key, imageData: imageData, presignedURLString: response.url)
-        } catch(let error) {
-            
-            throw GentiError.uploadFail(code: "AWS", message: "AWS에 한장 업로드 실패\(error.localizedDescription)")
-        }
+        let fileName = UUID().uuidString + ".JPG"
+        let response: GetUploadImageUrlDTO = try await self.requsetService.fetchResponse(for: GeneratorRouter.getPresignedUrl(fileName: fileName))
+        return try await uploadS3(dto: response, phAsset: phAsset)
     }
     
     func getS3Key(from phAssets: [PHAsset]) async throws -> [String] {
-        do {
-            let fileNames = phAssets.map { $0.value(forKey: "filename") as! String }
-            let responses: [GetUploadImageUrlDTO] = try await self.requsetService.fetchResponse(for: GeneratorRouter.getPresignedUrls(fileNames: fileNames))
-            return try await withThrowingTaskGroup(of: String?.self) { group in
-                for (dto, phAsset) in Array(zip(responses, phAssets)) {
-                    group.addTask {
-                        let imageData = try await self.imageDataTransferService.requestImageData(for: phAsset)
-                        return try await self.uploadService.upload(s3Key: dto.s3Key, imageData: imageData, presignedURLString: dto.url)
-                    }
-                }
-                
-                return try await group
-                    .reduce(into: [String?](), {$0.append($1)})
-                    .compactMap{$0}
-            }
-        } catch(let error) {
-            throw GentiError.uploadFail(code: "AWS", message: "AWS에 여러 장 업로드 실패 \(error.localizedDescription)")
-        }
+        let fileNames = phAssets.map { _ in UUID().uuidString + ".JPG" }
+        let responses: [GetUploadImageUrlDTO] = try await self.requsetService.fetchResponse(for: GeneratorRouter.getPresignedUrls(fileNames: fileNames))
+        
+        async let firstFaceImage = uploadS3(dto: responses[0], phAsset: phAssets[0])
+        async let secondFaceImage = uploadS3(dto: responses[1], phAsset: phAssets[1])
+        async let thirdFaceImage = uploadS3(dto: responses[2], phAsset: phAssets[2])
+        
+        return try await [firstFaceImage, secondFaceImage, thirdFaceImage].compactMap{$0}
+    }
+    
+    func uploadS3(dto: GetUploadImageUrlDTO, phAsset: PHAsset) async throws -> String? {
+        let imageData = try await self.imageDataTransferService.requestImageData(for: phAsset)
+        return try await self.uploadService.upload(s3Key: dto.s3Key, imageData: imageData, presignedURLString: dto.url)
     }
     
     func requestGenerateImage(prompt: String, poseURL: String?, faceURLs: [String], angle: PhotoAngle, coverage: PhotoFrame, ratio: PhotoRatio) async throws {
